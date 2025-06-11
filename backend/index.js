f5783a319420c1086ad666e1df312d41889cfdc6
Laksana Muhammad Agung <agunglefroed@gmail.com>
2025-06-11 17:40:43 +0700

    Fixing workflow 2.0

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+//
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
